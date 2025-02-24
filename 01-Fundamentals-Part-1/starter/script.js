220805e9b4e014808c4dbcdc14bf27a1e28cd2cd
Kyeongrae Noh <nohkr160428@gmail.com>
2025-02-24 22:14:04 +0900

    Completed to lecture 10 : Practice Assignments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,33 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("kynoh");
+console.log(23);
+
+let firstName = "firefox"; // camalCase is JS convention
+let first = "kynoh";
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let kynoh_javascript = "javascript";
+let $function = 27;
+
+let name = 3; // name will be reserved keyword, not recommended
+
+
+let Person = "kynoh"; // Upper case is for constant and OOP
+let PI = 3.141592;
+
+let job1 = "programmer";
+let myJob = "programmer";
+
+let $isValid = true;
+let $_isValid = true;
+//let %isValid = false;
+//let &isValid = false;
+/*
+`~!@#%^&*()-+=[]{}\|;:'",<.>/? 이 특수문자들은 변수명으로 지정 불가
+*/
